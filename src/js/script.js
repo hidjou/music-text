49e2a37fe1a75b250990ead804d5b335da1a3076
Hidjou <thehidjou@yahoo.com>
2018-01-09 20:57:33 +0000

    dividing text into multiple spans with their respective line number as a class, then joining spans together to form spans that are the whole line, process repeats on window resize

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,8 @@ function SelectText(element) {
         text = doc.getElementById(element),
         range,
         selection;    
-    if (doc.body.createTextRange) {
-        // Research this method
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();        
+    if (window.getSelection) {
+        selection = window.getSelection();
         range = document.createRange();
         range.selectNodeContents(text);
         selection.removeAllRanges();
@@ -18,6 +13,7 @@ function SelectText(element) {
     }
 }
 //string.join(str) - joins an array of strings and puts str between each
+// Original code
 // $(function($) {
 //     var lasty;
 
@@ -38,6 +34,7 @@ function SelectText(element) {
 //       }
 //     });
 // });​
+
 
 // // Change color of the first word of the line
 // // #00ffb4 = rgb(0, 255, 180)
@@ -60,33 +57,37 @@ function SelectText(element) {
 //     });
 // });
 
+
 // Change color of separate lines
-$(function($){
-    var oldTop, currentTop;
-    var $selected = $('#selectme');
+// $(function($){
+//     // positions
+//     var oldTop = -1, currentTop = -1;
+//     var color;
+//     var gradiant = 0;
 
-    var gradiant = 0;
-    var color = "rgb(0, 255, " + (180 - gradiant) + ")"
+//     // Get IDed element
+//     var $selected = $('#selectme');
+//     // Divide element into multiple spans
+//     $selected.html("<span>" + $selected.text().split(' ').join("</span> <span>") + "</span>");
 
-    $selected.html("<span>" + $selected.text().split(' ').join("</span> <span>") + "</span>");
-    oldTop = -1;
-    currentTop = -1;
-    
-    $selected.find('span').each(function(){
-        currentTop = $(this).position().top;
-        if(currentTop > oldTop){
-            oldTop = currentTop;
-            gradiant = gradiant + 10;
-            color = "rgb(0, 255, " + (180 - gradiant) + ")"
-            $(this).css("color", color);
-        } else {
-            $(this).css("color", color);
-        }
-    });
-});
+//     $selected.find('span').each(function(){
+//         currentTop = $(this).position().top;
+//         // If currentTop > oldTop => change of YPost => change of line
+//         // Change oldTop and color an
+//         if(currentTop > oldTop){
+//             oldTop = currentTop;
+//             color = "rgb(0, 255, " + (180 - gradiant) + ")"
+//             gradiant = gradiant + 10;
+//             $(this).css("color", color);
+//         } else {
+//             // If on the same line apply the same color
+//             $(this).css("color", color);
+//         }
+//     });
+// });
 
-// Make each line into its own span
 
+//// Original code
 // $(function(){
 //     var p = $('p'); 
 //     var words = p.text().split(' '); 
@@ -114,7 +115,75 @@ $(function($){
 //     $(window).resize(); //first one
 // });
 
+// takes all paragraphs and separates all words into spans, each span has class linex
+// where x = number of line, works on window resize
+// $(function(){
+//     // Get all paragraphs
+//     var myText = $('#selectme');
+//     myText.html('<span>' + myText.text().split(' ').join('</span> <span>') + '</span>');
+//     $(window).resize(function(){
+//         // Each time window resizes execute
+//         var line = 0; 
+//         var oldTop = -15;
+//         $('span', myText).each(function(){
+//             console.log('giving spans classes');
+//             var top = $(this).offset().top;
+//             if(top!=oldTop){
+//                 oldTop=top;
+//                 line++;
+//             }
+//             $(this).attr('class', 'line' + line);
+//         });//each
+//     });//resize
+//     $(window).resize(); //first one
+// });
+// Make each line into its own span
+$(function(){
+    // Get all paragraphs
+    var myText = $('#selectme');
+    $(window).resize(function(){
+        // Each time window resizes execute
+        myText.html('<span>' + myText.text().split(' ').join('</span> <span>') + '</span>');
+        var line = 0; 
+        var oldTop = -15;
+        var length = 0;
+        //console.log('dividing text');
+        // Give each span its respective line number
+        $('span', myText).each(function(){
+            length += 1;
+            var top = $(this).offset().top;
+            if(top!=oldTop){
+                oldTop=top;
+                line++;
+            }
+            $(this).attr('class', 'line' + line);
+        });//each
+        oldTop = -15;
+        var previousLine = 1;
+        var currentLine = 1;
+        var lineText = '<span class="line1">';
+        var index = 0;
+        $('span', myText).each(function(){
+            // The last span
+            if(index >= length - 1){
+                lineText += $(this).text() + '</span>';
+                return true;
+            }
+            currentLine = $(this).attr('class').substring(4);
+            if(currentLine == previousLine){
+                lineText += $(this).text() + ' ';
 
+            } else {
+                lineText += '</span><span class="line' + currentLine + '">' + $(this).text() + ' ';
+                previousLine += 1;
+            }
+            index +=1;
+        })
+        myText.html(lineText);
+    });//resize
+    $(window).resize(); //first one
+    alert('no errors');
+});
 
 // If anything inside the document is clicked
 document.onclick = function(e) {
